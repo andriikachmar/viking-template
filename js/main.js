631d8e8eccd07d963f9325c9f318261586180be7
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2020-10-30 13:40:36 +0200

    Revert "fix vh"

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
 $(function () {
 
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-    // We listen to the resize event
-    window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-
     $('.burger').on('click', function () {
         $('.burger').toggleClass('burger_active')
         $('.menu__list').toggleClass('menu__list--active')
     });
+
+
 
     $('.heroes__slider-img').slick({
         slidesToShow: 1,
@@ -44,3 +34,4 @@ $(function () {
         arrows: false
     });
 });
+
